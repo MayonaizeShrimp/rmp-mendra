d425dab3531e446f7d491f3722264154bd8b5695
MayonaizeShrimp <85448449+MayonaizeShrimp@users.noreply.github.com>
2023-11-18 14:05:10 +0700

    sample searchbar

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,27 +1,55 @@
-import { Button, Col, Row } from 'antd';
+import { Button, Card, Col, Flex, Input, Row } from 'antd';
 import { SampleTable } from './partials/sample-table';
+import { UserAddOutlined } from "@ant-design/icons"
+import Meta from 'antd/es/card/Meta';
+
+const PatientCard = (props: {name: string, dob: string, uuid: string}) => <Card hoverable>
+    
+    <Meta 
+        title={`${props.name} - ${props.dob}`}
+        description={props.uuid}/>
+</Card>
 
 function App() {
-    return <Row>
-        <Col span={4}>
-            <Button
-                type='primary'
-                children="button 1"/>
-            <Button
-                type='default'
-                children="button 2"/>
-            <Button
-                type='dashed'
-                children="button 3"/>
-            <Button
-                type='text'
-                children="button 4"/>
-            <Button
-                type='link'
-                children="button 5"/>
+    return <Row gutter={16}>
+        <Col span={5}>
+            <Flex vertical style={{height: "100vh"}}>
+                <Flex gap={8}>
+                    <Input.Search type='text' placeholder='Cari Pasien' size='large' allowClear/>
+                    <Button type='primary' size='large'><UserAddOutlined/> Pasien Baru</Button>
+                </Flex><br/>
+                <Flex  
+                    style={{height: "100%"}}
+                    gap={4}
+                    vertical={true}
+                    children={[
+                        <PatientCard name={'Budi Budi Budi Budi'} dob={'2023-08-08'} uuid={'s'}/>,
+                        <PatientCard name={'Charlie Charlie Charlie Charlie Charlie Charlie'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Dimas Uwu'} dob={'asdf'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                        <PatientCard name={'Emil'} dob={'2023-08-08'} uuid={'asdfsadf'}/>,
+                    ]}/>
+            </Flex>
         </Col>
-        <Col span={20}>
+        <Col span={14}>
             <SampleTable/>
+        </Col>
+        <Col span={5}>
         </Col>
     </Row>
 }
