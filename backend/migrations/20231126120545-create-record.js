585668738e34e064d997bf7564ff1c8c067ace12
MayonaizeShrimp <85448449+MayonaizeShrimp@users.noreply.github.com>
2023-12-16 17:17:07 +0700

    add current_timestamp as def value on migration

--- backend/migrations/20231126120545-create-record.js
+++ backend/migrations/20231126120545-create-record.js
@@ -11,6 +11,7 @@ module.exports = {
       },
       tanggal: {
         type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
       },
       tinggiBadan: {
         type: Sequelize.INTEGER
@@ -45,11 +46,13 @@ module.exports = {
       },
       createdAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
       },
       updatedAt: {
         allowNull: false,
-        type: Sequelize.DATE
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.NOW,
       }
     });
   },
