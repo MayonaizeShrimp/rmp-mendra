62bd98d37093726a4167cdd1e1db5e0349d62928
MayonaizeShrimp <85448449+MayonaizeShrimp@users.noreply.github.com>
2023-12-22 20:34:13 +0700

    fix spacing

--- frontend/src/pages/main-page.tsx
+++ frontend/src/pages/main-page.tsx
@@ -5,7 +5,6 @@ import { MedicRecordForm } from "src/partials/medic-record-form";
 import { PatientList } from "src/partials/patient-list";
 
 export const MainPage = () => {
-
   const em = useEventManager();
   
   return (
@@ -28,13 +27,11 @@ export const MainPage = () => {
           />
       </Col>
       <Col span={em.inputMode === "record" ? 7 : 0}>
-        <Flex vertical gap={16} style={{ height: "95vh" }}>
-            <MedicRecordForm 
-              selectedRecord={em.selectedRecord} 
-              isLoading={false} 
-              onSubmit={em.handleSubmitRecord} 
-              onCancel={em.handleCloseRecord} />
-        </Flex>
+        <MedicRecordForm 
+          selectedRecord={em.selectedRecord} 
+          isLoading={false} 
+          onSubmit={em.handleSubmitRecord} 
+          onCancel={em.handleCloseRecord} />
       </Col>
     </Row>
   );
